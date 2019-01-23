51d10cb47154d438c7c85e33a9abdd331819ba74
aganezov <aganezov@cs.jhu.edu>
2019-01-23 12:06:20 -0500

    setting things up

---
+++ .travis/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_OS_NAME" = 'osx' ]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+    bash ~/miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    conda info -a
+    conda create -n test-environment python=$PYTHON
+    source activate test-environment
+fi
+
+if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    # Useful for debugging any issues with conda
+    conda info -a
+    conda create -n test-environment python=$TRAVIS_PYTHON_VERSION
+    source activate test-environment
+fi
